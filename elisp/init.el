b0a37d8b7b0dc1403a953d17b4f1c2d1c17a48d2
Dustin Lacewell <dlacewell@gmail.com>
2018-01-13 23:53:38 -0600

    Updates

--- elisp/init.el
+++ elisp/init.el
@@ -1,73 +1,53 @@
-;; source path - based on init file
-;; user path - pased on environment variable
+(setq debug-on-error 1)
+(setq custom-file (make-temp-file ""))
 
-(defun knowt-elisp-directory ()
-  (concat (getenv "KNOWT_SOURCE_DIRECTORY") "elisp/"))
-(message (format "knowt-elisp-directory: %s" (knowt-elisp-directory)))
+(setq user-init-file load-file-name)
 
-(defun user-init-file () (concat (knowt-elisp-directory) "elisp" "init.el"))
-(message (format "user-init-file: %s" (user-init-file)))
-
-(defun knowt-user-directory ()
-  (file-name-as-directory (or (getenv "KNOWT_USER_DIRECTORY") (expand-file-name "~/.knowt/"))))
+;; knowt user directory
+(defun knowt-user-directory () (or (getenv "KNOWT_USER_DIRECTORY")
+                                   (expand-file-name "~/.knowt/")))
 (message (format "knowt-user-directory: %s" (knowt-user-directory)))
 
-(defun knowt-emacs-directory ()
-  (file-name-as-directory (concat (knowt-user-directory) ".emacs.d/")))
-(message (format "knowt-emacs-directory: %s" (knowt-emacs-directory)))
+;; knowt emacs directory
+(setq user-emacs-directory (concat (knowt-user-directory) ".emacs.d/"))
+(message (format "user-emacs-directory set: %s" user-emacs-directory))
 
-(defun knowt-emacs-file (filename)
-  (concat (knowt-emacs-directory) (file-name-nondirectory filename)))
-(message (format "knowt-emacs-file: %s" (knowt-emacs-file "FOO")))
+;; knowt source directory
+(defun knowt-source-directory () (file-name-directory load-file-name))
 
 (defun knowt-user-file (filename)
   (concat (knowt-user-directory) (file-name-nondirectory filename)))
 (message (format "knowt-user-file: %s" (knowt-user-file "FOO")))
 
-(defun knowt-mode-org-file ()
-  (concat (knowt-elisp-directory) "knowt-mode.org"))
-(message (format "knowt-mode-org-file: %s" (knowt-mode-org-file)))
-
-(defun knowt-mode-el-file ()
-  (concat (knowt-emacs-directory) "knowt-mode.el"))
-(message (format "knowt-mode-el-file: %s" (knowt-mode-el-file)))
-
 (defun knowt-user-org-file ()
-  (concat (knowt-emacs-directory) "init.org"))
+  (concat (knowt-user-directory) "init.org"))
 (message (format "knowt-user-org-file: %s" (knowt-user-org-file)))
 
 (defun knowt-user-el-file ()
-  (concat (knowt-emacs-directory) "init.el"))
+  (concat user-emacs-directory "init.el"))
 (message (format "knowt-user-el-file: %s" (knowt-user-el-file)))
 
-(defun knowt-build-el-file ()
-  (concat (knowt-elisp-directory) "org-build.el"))
-(message (format "knowt-build-el-file: %s" (knowt-build-el-file)))
+(defun knowt-org-file ()
+  (concat (knowt-user-directory) "knowt.org"))
+(message (format "knowt-org-file: %s" (knowt-org-file)))
 
-(setq user-init-file load-file-name)
-(setq user-emacs-directory (knowt-emacs-directory))
+(require 'org)
 
-(load-file (knowt-build-el-file))
+(defun knowt-build-core ()
+  (let ((core-org (concat (knowt-source-directory) "core.org"))
+        (core-el (concat user-emacs-directory "core.el")))
+    (org-babel-tangle-file core-org core-el)
+    (if (file-exists-p core-el)
+        (load-file core-el))))
 
-(require 'org-build)
-(build-custom)
-(build-mode)
+(defun knowt-build-custom ()
+  (if (file-exists-p (knowt-user-org-file))
+      (progn
+        (org-babel-tangle-file (knowt-user-org-file) (knowt-user-el-file))
+        (if (file-exists-p (knowt-user-el-file))
+            (load-file (knowt-user-el-file))))))
 
-(defun knowt-hook ()
-  (when (string= (file-name-nondirectory (buffer-file-name)) "knowt.org")
-    (knowt-mode 1)))
+(knowt-build-core)
+(knowt-build-custom)
 
-(add-hook 'find-file-hook 'knowt-hook)
-(find-file (knowt-user-file "knowt.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (req-package use-package el-get))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(find-file (knowt-org-file))
